53ea4e8dc740114f801cf50f23c74fea5373d0bd
Ahmed Omran <a.moh.omran@gmail.com>
2025-08-23 00:07:17 +0300

    fixing vite configs for correct paths in build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/personal-website/', // This makes paths work on GitHub Pages
+  base: '', // This makes paths work on GitHub Pages
 })
